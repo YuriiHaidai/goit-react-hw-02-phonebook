3795d817c404600a65e550b4ad14194d3b356201
YuriiHaidai <ura.kiev@gmail.com>
2020-07-26 13:20:42 +0300

    some fixes

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
+import styles from './App.module.css';
 
 export default class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
 
     filter: '',
@@ -54,7 +55,7 @@ export default class App extends Component {
     const contactsList = this.filteredContacts();
 
     return (
-      <div>
+      <div className={styles.App}>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h1>Contacts</h1>
