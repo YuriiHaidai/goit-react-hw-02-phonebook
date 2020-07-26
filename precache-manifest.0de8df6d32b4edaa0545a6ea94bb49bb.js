9d52f66620e6744dc50286772072113fcdddf5b7
YuriiHaidai <ura.kiev@gmail.com>
2020-07-26 13:28:57 +0300

    Updates

---
+++ precache-manifest.0de8df6d32b4edaa0545a6ea94bb49bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94b96ba04386e20f1057845439c1ed",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "9f6be41efeedc4dcaf91",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.c5f52fc0.chunk.css"
+  },
+  {
+    "revision": "ecbdea73a22e6fe20f8b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.274e56ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.274e56ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6be41efeedc4dcaf91",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.51ae8b56.chunk.js"
+  },
+  {
+    "revision": "de5dfbc78302bed1e7de",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3cc9cabd.js"
+  }
+]);
